a9861b4399921ce8c243bbe0fd0e81e00406cc8a
etctran <etctran@unc.edu>
2025-07-22 23:55:58 -0400

    prepared for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./pages/index.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -7,6 +7,10 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 import toast from "react-hot-toast";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "NoteCard";
+  }, []);
+
   return (
     <div className="relative h-full w-full">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#0080FF40_100%)]" />
